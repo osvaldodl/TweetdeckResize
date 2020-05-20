8d90923a20c70fcad2fa9194bdeac6ed53b7d5cf
Osvaldo Lima <osvaldo_lima@outlook.com>
2020-05-20 16:05:04 -0300

    first commit

---
+++ contentscript.js
@@ -0,0 +1,28 @@
+function update() {
+    let sections = document.getElementsByTagName('section');
+    for(i=0; i<sections.length;i++){
+        sections[i].setAttribute("style", "resize: horizontal; overflow: auto; ");
+    }
+}
+
+
+var observer = new MutationObserver(function(mutations) {
+  update();  
+});
+
+var config = { attributes: true, childList: true, characterData: true };
+
+function observ(){
+	let column = document.getElementsByClassName("app-columns")[0]; 
+	
+	console.log(column);
+	
+	if(!column){		
+		 window.setTimeout(observ,500)
+		return 
+	}	
+	update();
+	observer.observe(column, config);
+}
+
+observ();
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Tweetdeck Resize
+
+This extension permits you resize all columns in tweetdeck site
+
+## Instalation
+
+To install go to extensions on Google Chrome enable develop mode and select the folder. 
+
+Now when you access tweetdeck site will be a incon on the botton of all columns when you can resize.
+
+### How to install a extension on developer mode.
+
+[Tutorial how to install a unpacked extension](https://stackoverflow.com/questions/24577024/install-chrome-extension-not-in-the-store)
+
+
+
